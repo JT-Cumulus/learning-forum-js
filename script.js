62256a3d8f1a6b43c852f5fc6e22b91117c29162
Jiatong <jiatong1998@yahoo.com>
2023-08-09 21:35:33 +0200

    complete css look of webpage

---
+++ script.js
@@ -0,0 +1,69 @@
+console.log("Hello world");
+
+//Selecting DOM elements
+const btn = document.querySelector(".btn-open");
+const form = document.querySelector(".fact-form");
+const factsList = document.querySelector('.facts-list');
+
+// Create DOM elements
+factsList.innerHTML = "";
+
+function createFactsList(dataArray) {
+
+    const htmlArr = dataArray.map((fact) => `<li class="fact">
+        <p>
+        ${fact.text}
+        <a class="source" href="${fact.source}" target="_blank">(Source)</a>
+        </p>
+        <span class="tag" style="background-color: ${CATEGORIES.find((cat) => cat.name ===
+            fact.category).color};">${fact.category}</span>
+        </li>`
+    );
+
+    const html = htmlArr.join("");
+    factsList.insertAdjacentHTML("afterbegin", html);
+}
+
+// Load data from Supabase
+loadfacts();
+
+async function loadfacts() {
+    const res = await fetch("https://bigkumsgiwmafexnjvut.supabase.co/rest/v1/facts", {
+    headers: {
+        apikey: 
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJpZ2t1bXNnaXdtYWZleG5qdnV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTEzNDQyODMsImV4cCI6MjAwNjkyMDI4M30.TIp5LsrYOl6a3yJXFe2YktwIOvQlg0iCXSaIm5K75UI",
+        authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJpZ2t1bXNnaXdtYWZleG5qdnV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTEzNDQyODMsImV4cCI6MjAwNjkyMDI4M30.TIp5LsrYOl6a3yJXFe2YktwIOvQlg0iCXSaIm5K75UI",
+    }
+});
+
+const data = await res.json();
+createFactsList(data);
+}
+
+
+// Toggle form visibility
+btn.addEventListener('click', function() {
+    if(form.classList.contains('hidden')) {
+        form.classList.remove('hidden');
+        btn.textContent = "Close";
+    } else {
+        form.classList.add("hidden");
+        btn.textContent = "Share a fact";
+    }
+});
+
+
+
+const CATEGORIES = [
+    { name: "technology", color: "#3b82f6" },
+    { name: "science", color: "#16a34a" },
+    { name: "finance", color: "#ef4444" },
+    { name: "society", color: "#eab308" },
+    { name: "entertainment", color: "#db2777" },
+    { name: "health", color: "#14b8a6" },
+    { name: "history", color: "#f97316" },
+    { name: "news", color: "#8b5cf6" },
+  ];
+
+  
